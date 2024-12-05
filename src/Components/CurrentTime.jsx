bcaa43013a04044873c354baf8c60dd53c813d84
mithra905 <125637193+mithra905@users.noreply.github.com>
2024-12-05 14:46:47 +0530

    Initial commit

---
+++ src/Components/CurrentTime.jsx
@@ -0,0 +1,11 @@
+function CurrentTime() {
+
+    let time = new Date();
+
+    return (
+        <p className="lead">This is the time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+    )
+}
+
+
+    export default CurrentTime;
